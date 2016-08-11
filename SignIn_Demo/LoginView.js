49e9f909e0d76bc55ef59bd19d33ce24766afeb0
eingyinag <eingyin.ucsp@gmail.com>
2016-08-11 16:44:47 +0630

    Weather Forecast

--- SignIn_Demo/LoginView.js
+++ SignIn_Demo/LoginView.js
@@ -11,64 +11,66 @@ import {
 } from 'react-native';
 
 var SecureView = require("./SecureView");
+//import SecureView from './SecureView';
 
 class LoginView extends Component {
-	constructor(props) {
-		super(props);
-		this.state = {
-			username: "",
-			password: ""
-		};
-	}
 
-	render() {
-		return (
-			<View style={styles.container}>
-				<Text style={styles.title}> Sign_In </Text>
+    constructor(props) {
+        super(props);
+        this.state = {
+            username: "",
+            password: "",
+        };
+    }
 
-				<View>
-					<TextInput
-						placeholder="Username"
-						placeholderTextColor="blue"
-						style={styles.formInput}
-						onChange={(event) => this.setState({username: event.nativeEvent.text})}
-						value={this.state.username}
-						/>
-					<TextInput
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text style={styles.title}>
+                    Sign In
+                </Text>
+                <View>
+                    <TextInput
+                        placeholder="Username"
+                        onChange={(event) => this.setState({username: event.nativeEvent.text})}
+                        style={styles.formInput}
+                        value={this.state.username} />
+                    <TextInput
                         placeholder="Password"
                         secureTextEntry={true}
                         onChange={(event) => this.setState({password: event.nativeEvent.text})}
                         style={styles.formInput}
                         value={this.state.password} />
-					<TouchableHighlight onPress={(this.onSubmitPressed.bind(this))} style={styles.button}>
+                    <TouchableHighlight onPress={(this.onSubmitPressed.bind(this))} style={styles.button}>
                         <Text style={styles.buttonText}>Submit</Text>
                     </TouchableHighlight>
-				</View>
-			</View>
-			);
-	}
+                </View>
+            </View>
+        );
+    }
 
-	onSubmitPressed() {
+    onSubmitPressed() {
         this.props.navigator.push({
             title: "Secure Page",
             component: SecureView,
             passProps: {username: this.state.username, password: this.state.password}
         });
     }
-}
 
-const styles = StyleSheet.create({
-  container: {
+};
+
+var styles = StyleSheet.create({
+    container: {
         padding: 30,
         marginTop: 65,
-        alignItems: "stretch"
+//        alignItems: "stretch"
     },
     title: {
         fontSize: 18,
         marginBottom: 10
     },
     formInput: {
-        height: 36,
+        height: 50,
         padding: 10,
         marginRight: 5,
         marginBottom: 5,
@@ -79,7 +81,22 @@ const styles = StyleSheet.create({
         borderColor: "#555555",
         borderRadius: 8,
         color: "#555555"
-    }
+    },
+    button: {
+        height: 36,
+        flex: 1,
+        backgroundColor: "#555555",
+        borderColor: "#555555",
+        borderWidth: 1,
+        borderRadius: 8,
+        marginTop: 10,
+        justifyContent: "center"
+    },
+    buttonText: {
+        fontSize: 18,
+        color: "#ffffff",
+        alignSelf: "center"
+    },
 });
 
 module.exports = LoginView;
--- SignIn_Demo/SecureView.js
+++ SignIn_Demo/SecureView.js
@@ -4,38 +4,57 @@ import React, {Component} from 'React';
 import {
 	StyleSheet,
 	Text,
-    View
+    View,
+    BackAndroid,
+    Image,
 } from 'react-native';
+
+var WeatherProject = require("./WeatherProject");
 
 class SecureView extends Component {
 	constructor(props) {
-        super(props);
-        this.state = {
-            username: this.props.username,
-            password: this.props.password
-        };
-    }
+    		super(props);
+    		this.state = {
+    			username: this.props.username,
+    		};
+    	}
 
     render() {
     	return (
-            <View style={styles.container}>
+    	<View style={styles.container}>
+            <View >
                 <Text style={styles.heading}>
-                    Welcome {this.state.username}!
+                    Welcome {this.props.username}!
                 </Text>
+                <Image source={{uri: 'https://facebook.github.io/react/img/logo_og.png'}}/>
+
             </View>
+            <View>
+                <WeatherProject/>
+            </View>
+        </View>
         );
     }
 }
 
+BackAndroid.addEventListener('hardwareBackPress', () => {
+  if (this.props.navigator.getCurrentRoutes().length === 1) {
+    return false;
+  }
+
+  this.props.navigator.pop();
+  return true;
+})
+
 const styles = StyleSheet.create({
   container: {
-        padding: 30,
+        padding: 50,
         marginTop: 65,
         alignItems: "center"
     },
     heading: {
         marginBottom: 20,
-        fontSize: 18,
+        fontSize: 20,
         textAlign: "center",
         color: "#656565"
     }
